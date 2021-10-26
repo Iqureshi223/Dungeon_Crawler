35aa1c04b3f74479a80a4cb5b20ab1e0ddf7c41a
Imaad <Iqureshi@mail.umw.edu>
2021-10-26 14:50:59 +0000

    added in input and ansi terminal

---
+++ Input.java
@@ -0,0 +1,19 @@
+import ansi_terminal.*;
+public class Input {
+String input = "";
+public Input() {
+
+}
+public String getInput() {
+
+}
+
+public void openIventory(Entity entity) {
+
+}
+public void help() {
+
+
+}
+
+}
