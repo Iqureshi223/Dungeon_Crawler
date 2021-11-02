aa1353cdfeb1a1122e23e61316ea983653c79297
Cooper Fulton <cooperfulton2020@gmail.com>
2021-11-02 21:35:44 +0000

    Created a somewhat basis for main

---
+++ MainCrawler.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MainCrawler
+{
+	public static void main (String [] args)
+	{
+		Scanner in = new Scanner(System.in);
+		Map map = new Map();
+
+		System.out.println("Welcome to Dungeon Crawlers");
+	}
+}
