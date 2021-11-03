c5add8c16344a5d2ddcb532b6f6cadbf9cf94871
Bryan Potts <bpotts@mail.umw.edu>
2021-11-03 02:47:06 +0000

    added new file 'MainTest.java'. this file is what I used to test the map class, feel free to use it if you need

---
+++ MainTest.java
@@ -0,0 +1,17 @@
+/* This class only exists to test the other classes.
+ * Not intended for final version.
+ */
+public class MainTest{
+	
+	public static void main(String[] args){
+		
+		//create and run a map object
+		Map map = new Map();
+
+		//map.printMap();
+		//map.createEntity();
+		//map.createEntity();
+		map.moveAll();
+	}
+}
+
