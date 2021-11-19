2ae4ce7a5adc2fa8e729ccb270a91fa1ae3fe3fb
Bryan Potts <bpotts@mail.umw.edu>
2021-11-19 21:04:34 +0000

    changes to map and edited ItemEntity

--- ItemEntity.java
+++ ItemEntity.java
@@ -1,12 +1,13 @@
-
 public class ItemEntity extends Entity
 {
 	private boolean isItem;
+	private boolean isStairs; //added -Bryan
 
 	public ItemEntity()
 	{
 		super();
 		isItem = true;
+		isStairs = false; //added -Bryan
 		
 	}
 
@@ -15,8 +16,21 @@ public class ItemEntity extends Entity
 		return isItem;
 	}
 
-	public void setIsItem(itemSet)
+	public void setIsItem(boolean itemSet) //fixed -Bryan
 	{
 		isItem = itemSet;
+	}
+
+	//added -Bryan
+	public boolean getIsStairs(){
+		
+		return isStairs;
+	}
+
+	//added -Bryan
+	public void setIsStairs(boolean setStairs){
+		
+		isStairs = setStairs;
+
 	}
 }
